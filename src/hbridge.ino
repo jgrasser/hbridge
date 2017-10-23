0ee8a6516cd46a29475586b6e58630a75c8caa16
Joseph Grasser <jgrasser.jge@gmail.com>
2017-10-22 19:32:32 -0700

    First commit

---
+++ src/hbridge.ino
@@ -0,0 +1,41 @@
+/*
+ * Project hbridge
+ * Description: Create H-Bridge Motor Controller using Particle Relay Shield
+ * Author: Joseph Grasser
+ * Date: 10/22/2017
+ */
+
+int RELAY1 = D3;
+int RELAY2 = D4;
+int RELAY3 = D5;
+int RELAY4 = D6;
+int BUTTON = D1;
+
+void setup()
+{
+    //Initilize the relay control pins as output
+    pinMode(RELAY1, OUTPUT);
+    pinMode(RELAY2, OUTPUT);
+    pinMode(RELAY3, OUTPUT);
+    pinMode(RELAY4, OUTPUT);
+    pinMode(BUTTON, INPUT_PULLUP);  
+
+    // Initialize all relays to an OFF state
+    digitalWrite(RELAY1, LOW);
+    digitalWrite(RELAY2, LOW);
+    digitalWrite(RELAY3, LOW);
+    digitalWrite(RELAY4, LOW);
+}
+
+void loop()
+{
+    if (digitalRead(BUTTON) == LOW)
+    {
+        digitalWrite(RELAY1, HIGH);
+        digitalWrite(RELAY2, LOW);
+    }
+    else{
+        digitalWrite(RELAY1, LOW);
+        digitalWrite(RELAY2, HIGH);
+    }
+}
